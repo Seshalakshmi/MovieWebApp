28f0bf4ebe3613cc3bbb34e5d0c244ff5ec65f56
sesha <sesha@yahoo.com>
2026-06-10 17:02:59 +0200

    Initialize app and database

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask
+from data_manager import DataManager
+from models import db, User, Movie, UserMovies
+import os
 
 app = Flask(__name__)
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/movies.db')}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)  # Link the database and the app. This is the reason you need to import db from models
+
+data_manager = DataManager() # Create an object of your DataManager class
 
 
 if __name__ == "__main__":
